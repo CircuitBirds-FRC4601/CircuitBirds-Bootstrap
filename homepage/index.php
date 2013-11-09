8d34c8469cecbf5777e962ec43ddcc9ccea25ee6
Paul <ugolinipaul@gmail.com>
2013-11-08 19:19:03 -0500

    Better footer and glyphicons

--- homepage/index.php
+++ homepage/index.php
@@ -4,7 +4,7 @@
 		<title>CircuitBirds 4601</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href="css/bootstrap.css" rel="stylesheet" media="screen">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -33,34 +33,41 @@
 			</div>
 			<div class="container">
 				<div class="row">
-					<div class="col-md-8"><h3>Preparing for 2014 <span class="label label-default">New</span></h3><img src="http://circuitbirds.com/images/img_0493.png" width="100%" alt="" />After winning the Rookie All-star award, we got ourselves a spot in the championship! This picture was taken while cleaning up our pit area and includes almost all our team members.
+					<div class="col-md-8"><h3>Preparing for 2014 <span class="label label-danger">New</span></h3><img src="http://circuitbirds.com/images/img_0493.png" width="100%" alt="" />After winning the Rookie All-star award, we got ourselves a spot in the championship! This picture was taken while cleaning up our pit area and includes almost all our team members.
 					</div>
 					<div class="col-md-4"><h3>About</h3>We are rookie team from Canfield, Ohio. We attended the Pittsburgh, PA Regional, where we won the Highest Rookie Seed Award, Rookie All-star award, and were 5<sup>th</sup> based on the number of points. At the championship we won the Rookie Inspiration Award and placed 31<sup>st</sup> in the Galileo Division.
 					</div>
 					<div class="col-md-4">
 						<h3>Media and Contact</h3>
-						<ul>
-							<li><a href="http://www.youtube.com/user/CanfieldCircuitBirds" target="_blank">YouTube</a></li>
-							<li><a href="http://twitter.com/team4601" target="_blank">Twitter</a></li>
-							<li><a href="https://plus.google.com/b/103521770448679495124/photos/103521770448679495124/albums/5850979947374350561" target="_blank">Photos on Google Plus</a>
-							</li>
-							<li><a href="/contact/">Email Us</a></li>
-						</ul>
+							<div class="row">
+							<div class="col-md-11 pull-right"><img src="fonts/glyphicons_youtube.png" height="24" width="24" border="0"/> &nbsp;&nbsp;
+							<a href="http://www.youtube.com/user/CanfieldCircuitBirds" target="_blank">YouTube</a>
+							</div>
+							
+							<div style="padding-top:10px;" class="col-md-11 pull-right"><img src="fonts/glyphicons_twitter.png" height="24" width="24" border="0"/>&nbsp;&nbsp;<a href="http://twitter.com/team4601" target="_blank">Twitter</a>
+							</div>
+							
+							<div style="padding-top:10px;" class="col-md-11 pull-right"><img src="fonts/glyphicons_google_plus.png" height="24" width="24" border="0"/>&nbsp;&nbsp;
+							<a href="https://plus.google.com/b/103521770448679495124/photos/103521770448679495124/albums/5850979947374350561" target="_blank">Photos on Google Plus</a>
+							</div>
+							
+							<div style="padding-top:10px;" class="col-md-11 pull-right"><img src="fonts/glyphicons_e-mail.png" height="24" width="24" border="0"/>&nbsp;&nbsp;<a href="/contact/">Email Us</a></div>
+							</div>
 					</div>
 					<div class="col-md-4">
 						<h3>Helpful Teams Near Us</h3>
-						<ul>
-							<li><a href="http://delphielite.com/" target="_blank">#48   - Delphi E.L.I.T.E.</a></li>
-							<li><a href="http://www.redbarons63.com/" target="_blank">#63   - The Red Barons</a></li>
-							<li><a href="http://team291.org/Team291/index.html" target="_blank">#291  - C.I.A. - Creativity In Action</a>
-							</li>
-							<li><a href="http://falcotech3193.com/" target="_blank">#3193 - Falco Tech</a></li>
-						</ul>
+						<div class="row">
+						<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-search"></span>&nbsp;&nbsp;<a href="http://delphielite.com/" target="_blank">#48   - Delphi E.L.I.T.E.</a></div>
+						<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-search"></span>&nbsp;&nbsp;<a href="http://www.redbarons63.com/" target="_blank">#63   - The Red Barons</a></div>
+						<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-search"></span>&nbsp;&nbsp;<a href="http://team291.org/Team291/index.html" target="_blank">#291  - C.I.A. - Creativity In Action</a></div>
+						<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-search"></span>&nbsp;&nbsp;<a href="http://falcotech3193.com/" target="_blank">#3193 - Falco Tech</a></div>
+					</div>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-md-4 col-md-offset-4">
-						Copyright&copy; 2013 &middot; CircuitBirds.com
+				<div style="padding-top:20px;" class="row">
+					<hr>
+					<div class="col-md-3 col-md-offset-4">
+						<div class="well pull-right well-sm"><span class="label label-default">Copyright&copy; 2013 &middot; CircuitBirds.com</span></div>
 					</div>
 				</div>
 			</div>
