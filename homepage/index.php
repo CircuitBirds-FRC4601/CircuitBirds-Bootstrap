ae1eb6c6264b0fd5aa473f08e8525544a97fe0a0
Paul <ugolinipaul@gmail.com>
2013-11-10 18:19:48 -0500

    Added External Link indicators, usfirst links, blue alliance link, and attempted tooltip for logo.

--- homepage/index.php
+++ homepage/index.php
@@ -21,13 +21,25 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">FRC #4601 &middot; Circuit Birds</a>
+					<a id="Title" class="navbar-brand" data-toggle="tooltip" title="first tooltip">FRC #4601 &middot; Circuit Birds</a>
 				</div>
 				<div class="navbar-collapse collapse">
 					<ul class="nav navbar-nav">
 						<li class="active"><a href="#">Home</a></li>
 						<li><a href="#">Sponsors</a></li>
 						<li><a href="#">Contact</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">The Blue Alliance <span class="glyphicon glyphicon-share"></span></a></li>
+						<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">usfirst.org <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="//www.usfirst.org/roboticsprograms/frc/resources">Resources <span class="glyphicon glyphicon-share"></span></a></li>
+							<li class="divider"></li>
+							<li class="disabled"><a href="#">2014 Game <span class="glyphicon glyphicon-share"></span></a></li>
+							<li class="disabled"><a href="#">2014 Rule Book <span class="glyphicon glyphicon-share"></span></a></li>
+							<li class="disabled"><a href="#">2014 <b>Safety</b> Manual <span class="glyphicon glyphicon-share"></span></a></li>
+						</ul>
 					</ul>
 				</div>
 			</div>
@@ -39,29 +51,31 @@
 					</div>
 					<div class="col-md-4">
 						<h3>Media and Contact</h3>
-							<div class="row">
-							<div class="col-md-11 pull-right"><img src="fonts/glyphicons_youtube.png" height="24" width="24" border="0"/> &nbsp;&nbsp;
-							<a href="http://www.youtube.com/user/CanfieldCircuitBirds" target="_blank">YouTube</a>
+						<div class="row">
+							<div class="col-md-11 pull-right"><img src="fonts/glyphicons_youtube.png" height="24" width="24" border="0"/>&nbsp;
+								<a href="http://www.youtube.com/user/CanfieldCircuitBirds" target="_blank">YouTube</a>
 							</div>
-							
 							<div style="padding-top:10px;" class="col-md-11 pull-right"><img src="fonts/glyphicons_twitter.png" height="24" width="24" border="0"/>&nbsp;&nbsp;<a href="http://twitter.com/team4601" target="_blank">Twitter</a>
 							</div>
-							
-							<div style="padding-top:10px;" class="col-md-11 pull-right"><img src="fonts/glyphicons_google_plus.png" height="24" width="24" border="0"/>&nbsp;&nbsp;
-							<a href="https://plus.google.com/b/103521770448679495124/photos/103521770448679495124/albums/5850979947374350561" target="_blank">Photos on Google Plus</a>
+							<div style="padding-top:10px;" class="col-md-11 pull-right"><img src="fonts/glyphicons_google_plus.png" height="24" width="24" border="0"/>&nbsp;
+								<a href="https://plus.google.com/b/103521770448679495124/photos/103521770448679495124/albums/5850979947374350561" target="_blank">Photos on Google Plus</a>
 							</div>
-							
-							<div style="padding-top:10px;" class="col-md-11 pull-right"><img src="fonts/glyphicons_e-mail.png" height="24" width="24" border="0"/>&nbsp;&nbsp;<a href="/contact/">Email Us</a></div>
+							<div style="padding-top:10px;" class="col-md-11 pull-right"><img src="fonts/glyphicons_e-mail.png" height="24" width="24" border="0"/>&nbsp;&nbsp;<a href="/contact/">Email Us</a>
 							</div>
+						</div>
 					</div>
 					<div class="col-md-4">
 						<h3>Helpful Teams Near Us</h3>
 						<div class="row">
-						<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-search"></span>&nbsp;&nbsp;<a href="http://delphielite.com/" target="_blank">#48   - Delphi E.L.I.T.E.</a></div>
-						<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-search"></span>&nbsp;&nbsp;<a href="http://www.redbarons63.com/" target="_blank">#63   - The Red Barons</a></div>
-						<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-search"></span>&nbsp;&nbsp;<a href="http://team291.org/Team291/index.html" target="_blank">#291  - C.I.A. - Creativity In Action</a></div>
-						<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-search"></span>&nbsp;&nbsp;<a href="http://falcotech3193.com/" target="_blank">#3193 - Falco Tech</a></div>
-					</div>
+							<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-share"></span>&nbsp;&nbsp;<a href="http://delphielite.com/" target="_blank">#48   - Delphi E.L.I.T.E.</a>
+							</div>
+							<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-share"></span>&nbsp;&nbsp;<a href="http://www.redbarons63.com/" target="_blank">#63   - The Red Barons</a>
+							</div>
+							<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-share"></span>&nbsp;&nbsp;<a href="http://team291.org/Team291/index.html" target="_blank">#291  - C.I.A. - Creativity In Action</a>
+							</div>
+							<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-share"></span>&nbsp;&nbsp;<a href="http://falcotech3193.com/" target="_blank">#3193 - Falco Tech</a>
+							</div>
+						</div>
 					</div>
 				</div>
 				<div style="padding-top:20px;" class="row">
@@ -72,7 +86,14 @@
 				</div>
 			</div>
 		</div><!--/CONTAINER-->
-	
+
+<script>
+$(function () {
+    $("#Title").tooltip({
+        placement : 'top'
+    });
+});
+</script>
 <script src="//code.jquery.com/jquery.js"></script>
 <script src="js/bootstrap.min.js"></script>
 	</body>
