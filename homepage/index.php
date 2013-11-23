457c313d65e808009fa645b12bd261c633db7d62
Paul <ugolinipaul@gmail.com>
2013-11-22 20:57:25 -0500

    Modal for login. Collapse for "posts" makes things look clean and compact.

--- homepage/index.php
+++ homepage/index.php
@@ -3,38 +3,41 @@
 	<head>
 		<title>CircuitBirds 4601</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
 		<link href="css/bootstrap.css" rel="stylesheet" media="screen">
-
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="../../assets/js/html5shiv.js"></script>
       <script src="../../assets/js/respond.min.js"></script>
     <![endif]-->
+		<style>
+hr {margin:8px;}
+		</style>
 	</head>
 	<body>
 		<div class="container">
-			<div class="navbar navbar-default">
+			<div class="navbar navbar-inverse navbar-default">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a id="Title" class="navbar-brand" data-toggle="tooltip" title="first tooltip">FRC #4601 &middot; Circuit Birds</a>
+					<a id="Title" class="navbar-brand">FRC #4601 &middot; Circuit Birds</a>
 				</div>
 				<div class="navbar-collapse collapse">
 					<ul class="nav navbar-nav">
 						<li class="active"><a href="#">Home</a></li>
 						<li><a href="#">Sponsors</a></li>
 						<li><a href="#">Contact</a></li>
+						<li><a onclick="ShowLoginModal()">Login</a></li>
 					</ul>
 					<ul class="nav navbar-nav navbar-right">
 						<li><a href="#">The Blue Alliance <span class="glyphicon glyphicon-share"></span></a></li>
 						<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown">usfirst.org <b class="caret"></b></a>
 						<ul class="dropdown-menu">
-							<li><a href="//www.usfirst.org/roboticsprograms/frc/resources">Resources <span class="glyphicon glyphicon-share"></span></a></li>
+							<li><a href="//www.usfirst.org/roboticsprograms/frc/resources">Resources <span class="glyphicon glyphicon-share"></span></a>
+							</li>
 							<li class="divider"></li>
 							<li class="disabled"><a href="#">2014 Game <span class="glyphicon glyphicon-share"></span></a></li>
 							<li class="disabled"><a href="#">2014 Rule Book <span class="glyphicon glyphicon-share"></span></a></li>
@@ -45,7 +48,41 @@
 			</div>
 			<div class="container">
 				<div class="row">
-					<div class="col-md-8"><h3>Preparing for 2014 <span class="label label-danger">New</span></h3><img src="http://circuitbirds.com/images/img_0493.png" width="100%" alt="" />After winning the Rookie All-star award, we got ourselves a spot in the championship! This picture was taken while cleaning up our pit area and includes almost all our team members.
+					<div class="col-md-8">
+						<div class="panel-group" id="accordion">
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+											Bake Sale!
+										</a><span class="label label-danger">New</span>
+									</h4>
+								</div>
+								<div id="collapseOne" class="panel-collapse collapse in">
+									<div class="panel-body">
+										<img src="http://circuitbirds.com/images/img_0493.png" width="100%" alt="" /><hr>
+										<div class="well">We will be at Canfield High School on December 7<sup>th</sup> for Canfield High School's annual Craft Show. Our table will be next to the CAD Lab, room 222, and baked goods of all kinds will be sold.
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+											On our Way to St. Louis!
+										</a>
+									</h4>
+								</div>
+								<div id="collapseTwo" class="panel-collapse collapse">
+									<div class="panel-body">
+										<img src="http://circuitbirds.com/images/img_0493.png" width="100%" alt="" /><hr>
+										<div class="well">After winning the Rookie All-star award, we got ourselves a spot in the championship! This picture was taken while cleaning up our pit area and includes almost all our team members.
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 					<div class="col-md-4"><h3>About</h3>We are rookie team from Canfield, Ohio. We attended the Pittsburgh, PA Regional, where we won the Highest Rookie Seed Award, Rookie All-star award, and were 5<sup>th</sup> based on the number of points. At the championship we won the Rookie Inspiration Award and placed 31<sup>st</sup> in the Galileo Division.
 					</div>
@@ -64,14 +101,14 @@
 							</div>
 						</div>
 					</div>
-					<div class="col-md-4">
+					<div class="col-md-4 pull-right">
 						<h3>Helpful Teams Near Us</h3>
 						<div class="row">
-							<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-share"></span>&nbsp;&nbsp;<a href="http://delphielite.com/" target="_blank">#48   - Delphi E.L.I.T.E.</a>
+							<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-share"></span>&nbsp;&nbsp;<a href="http://delphielite.com/" target="_blank">#48 - Delphi E.L.I.T.E.</a>
 							</div>
-							<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-share"></span>&nbsp;&nbsp;<a href="http://www.redbarons63.com/" target="_blank">#63   - The Red Barons</a>
+							<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-share"></span>&nbsp;&nbsp;<a href="http://www.redbarons63.com/" target="_blank">#63 - The Red Barons</a>
 							</div>
-							<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-share"></span>&nbsp;&nbsp;<a href="http://team291.org/Team291/index.html" target="_blank">#291  - C.I.A. - Creativity In Action</a>
+							<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-share"></span>&nbsp;&nbsp;<a href="http://team291.org/Team291/index.html" target="_blank">#291 - C.I.A. - Creativity In Action</a>
 							</div>
 							<div class="col-md-11 pull-right"><span class="glyphicon glyphicon-share"></span>&nbsp;&nbsp;<a href="http://falcotech3193.com/" target="_blank">#3193 - Falco Tech</a>
 							</div>
@@ -86,13 +123,47 @@
 				</div>
 			</div>
 		</div><!--/CONTAINER-->
+		
+		<!-- Modal -->
+		<div class="modal fade" id="LoginModal" tabindex="-1" role="dialog" aria-labelledby="LoginPopup" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h4 class="modal-title">Login</h4>
+					</div>
+					<div class="modal-body">
+						<div class="row">
+							<div class="col-md-12">Login to the team member area. If you are not a part of the team, Talk to Mr. Crum in room 222.<strong>This has not yet been implemented, so this does nothing.</strong><hr>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md12">
+							<form class="form">
+								<div class="well">
+									<div id="error-Wrapper"></div>
+									<div class="form-group">
+										<label class="control-label" for="FName">username</label>
+										<input type="text" class="form-control" id="FName" placeholder="Enter first name">
+									</div>
+									<div class="form-group">
+										<label class="control-label" for="LName">password</label>
+										<input type="text" class="form-control" id="LName" placeholder="Enter last name">
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-warning" data-dismiss="modal" onclick="HideLoginModal()">Cancel</button>
+					</div>
+				</div><!-- /MODAL-CONTENT -->
+			</div><!-- /MODAL-DIALOG -->
+		</div><!-- /MODAL -->
+
 
 <script>
-$(function () {
-    $("#Title").tooltip({
-        placement : 'top'
-    });
-});
+function ShowLoginModal(){$('#LoginModal').modal({backdrop:false})}
+function HideLoginModal(){$('#LoginModal').modal(hide)}
 </script>
 <script src="//code.jquery.com/jquery.js"></script>
 <script src="js/bootstrap.min.js"></script>
