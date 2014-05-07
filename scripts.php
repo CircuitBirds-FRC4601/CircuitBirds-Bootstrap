8371eff8a004352141134d4da6ba53dc3d1e4065
Paul Ugolini <ugolinipaul@gmail.com>
2014-05-07 12:56:21 -0400

    Added Google Tracking to scripts.php

--- scripts.php
+++ scripts.php
@@ -2,3 +2,4 @@
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <!--<script src="js/sha.js"></script>-->
 <script src="js/script.js"></script>
+<script>(function(e,t,n,r,i,s,o){e["GoogleAnalyticsObject"]=i;e[i]=e[i]||function(){(e[i].q=e[i].q||[]).push(arguments)},e[i].l=1*new Date;s=t.createElement(n),o=t.getElementsByTagName(n)[0];s.async=1;s.src=r;o.parentNode.insertBefore(s,o)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");ga("create","UA-39321366-2","circuitbirds.com");ga("send","pageview")</script>
