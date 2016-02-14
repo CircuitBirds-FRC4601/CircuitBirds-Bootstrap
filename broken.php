edf0d6c4b0517bc44c01229d0014464a6c645835
Paul Ugolini <pau4586@rit.edu>
2016-02-14 11:54:22 -0500

    Delete broken.php

--- broken.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", "HAI");
-?>
-<BR>
-<?php
-echo preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", "ugolinipaul@gmail.com");
-?>
-<img src="holder.js/300x200">
